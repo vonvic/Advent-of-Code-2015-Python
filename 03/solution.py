9dc0ce4841c739da8dc6a4988026007c3d0f7d9e
vonvic <40438077+vonvic@users.noreply.github.com>
2022-08-14 14:37:47 -0500

    Add template code for Problem 03

---
+++ 03/solution.py
@@ -0,0 +1,11 @@
+"""Advent of Code 2015 Problem 03 Solution."""
+
+
+def part_one_answer() -> int:
+    """Return part one answer."""
+    pass
+
+
+def part_two_answer() -> int:
+    """Return part two answer."""
+    pass
