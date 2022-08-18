95bccf01d490317f5b7fc22acf6d041abef2829e
vonvic <40438077+vonvic@users.noreply.github.com>
2022-08-18 07:04:10 -0500

    Implement Problem 04 Part Two Solution.
    Because part two was very similar to part one, I decided to modify the
    "at_least_five_zeroes" function to instead take an n amount of zeroes. This
    ensured code reuse.

--- 04/solution.py
+++ 04/solution.py
@@ -9,30 +9,30 @@ def __load_input(filename: str) -> str:
         return f.readline().strip()
 
 
-def __at_least_five_zeroes(inp: str) -> int:
+def __at_least_n_zeroes(inp: str, n: int) -> int:
     """
     Retunr the first number that produces a hash of a specified criteria.
 
-    The hash must must start with at least five zoeroes.
+    The hash must must start with at least n zoeroes.
     """
     i = 0
     msg_digest = ""
-    while not re.match(r"00000+", msg_digest):
+    pattern = f'{"0"*n}+'
+    while not re.match(pattern, msg_digest):
         i += 1
         s = f"{inp}{i}"
         msg_digest = hashlib.md5(s.encode("UTF-8")).hexdigest()
-
     return i
 
 
 def part_one_answer():
     """Return part one answer."""
-    return __at_least_five_zeroes(input_str)
+    return __at_least_n_zeroes(input_str, 5)
 
 
 def part_two_answer():
     """Return part two answer."""
-    pass
+    return __at_least_n_zeroes(input_str, 6)
 
 
 input_str = __load_input("04/input/input.txt")
